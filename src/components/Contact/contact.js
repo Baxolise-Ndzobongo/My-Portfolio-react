0b1bb1298354907710534dcb75864d60d5c15321
kwakhanya <baxolisendzobongo222@gmail.com>
2024-02-21 15:41:05 +0200

    styled contact and removed css on portfolio and expertise and replaced with correct one

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -66,6 +66,7 @@ const Contact = () => {
       <div className="circle-container">
         <div className="circle circle1"></div>
         <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
       </div>
       <div className="form-container"> 
         <form className="form" onSubmit={handleSubmit}>
