6f7984d5fee9fea1ba3f980b05fc41699d736d81
kwakhanya <baxolisendzobongo222@gmail.com>
2024-02-22 17:35:25 +0200

    added github links for projects

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -19,7 +19,9 @@ const Portfolio = () => {
                 <h2>Bank App</h2>
                 This is a bank app that allows users to log in and create accounts. After that, a user can make transactions, either withdrawal or make a deposit.
               </div>
-              <button className="portfolioButton">Github Link</button>
+              <a href="https://github.com/kwakhanya/Bank_App" target="_blank" rel="noopener noreferrer">
+                <button className="portfolioButton">Github Link</button>
+              </a>
             </div>
           </div>
 
@@ -30,7 +32,9 @@ const Portfolio = () => {
                 <h2>Tax Calculator</h2>
                 This tax calculator calculates the amount each person will be taxed based on income, age, and whether a person does qualify for tax.
               </div>
-              <button className="portfolioButton">Github Link</button>
+              <a href="https://github.com/your-username/tax-calculator" target="_blank" rel="noopener noreferrer">
+                <button className="portfolioButton">Github Link</button>
+              </a>
             </div>
           </div>
         </div>
@@ -43,7 +47,9 @@ const Portfolio = () => {
                 <h2>Investment Calculator</h2>
                 This is a calculator for investment or bonds, where a user can input principal, interest rate, and time.
               </div>
-              <button className="portfolioButton">Github Link</button>
+              <a href="https://github.com/kwakhanya/financial_calculator" target="_blank" rel="noopener noreferrer">
+                <button className="portfolioButton">Github Link</button>
+              </a>
             </div>
           </div>
 
@@ -54,7 +60,9 @@ const Portfolio = () => {
                 <h2>Construction Website</h2>
                 This is a construction website with real-time chat and a game for website visitors.
               </div>
-              <button className="portfolioButton">Github Link</button>
+              <a href=" https://kwakhanya.github.io/Thee-Best-Group-Project/" target="_blank" rel="noopener noreferrer">
+                <button className="portfolioButton">Github Link</button>
+              </a>
             </div>
           </div>
         </div>
