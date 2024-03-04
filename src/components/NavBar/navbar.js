76df53ada45e9553cb965087bf21853252ff6a5c
kwakhanya <baxolisendzobongo222@gmail.com>
2024-03-04 10:17:22 +0200

    remove unnecesary style

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -6,6 +6,7 @@ import './navbar.css';
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState('');
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -15,6 +16,10 @@ const Navbar = () => {
         setIsMenuOpen(false);
     };
 
+    const handleSetActive = (to) => {
+        setActiveLink(to);
+    };
+
     return (
         <div>
             <nav className={isMenuOpen ? 'navbar sticky open' : 'navbar sticky'}>
@@ -22,10 +27,50 @@ const Navbar = () => {
                     <div className="logo"><a href="#">Portfo<span>lio.</span></a></div>
                     <ul className={isMenuOpen ? 'menu open' : 'menu'}>
                         <div className="desktopMenu">
-                            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem" onClick={closeMenu}>HOME</Link>
-                            <Link activeClass='active' to='expertise' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem" onClick={closeMenu}>EXPERTISE</Link>
-                            <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem" onClick={closeMenu}>PORTFOLIO</Link>
-                            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-200} duration={500} className="desktopMenuListItem" onClick={closeMenu}>CONTACT</Link>
+                            <Link 
+                                to='intro' 
+                                spy={true} 
+                                smooth={true} 
+                                offset={-100} 
+                                duration={500} 
+                                className={`desktopMenuListItem ${activeLink === 'intro' ? 'active' : ''}`}
+                                onClick={() => { closeMenu(); handleSetActive('intro'); }}
+                            >
+                                HOME
+                            </Link>
+                            <Link 
+                                to='expertise' 
+                                spy={true} 
+                                smooth={true} 
+                                offset={-100} 
+                                duration={500} 
+                                className={`desktopMenuListItem ${activeLink === 'expertise' ? 'active' : ''}`}
+                                onClick={() => { closeMenu(); handleSetActive('expertise'); }}
+                            >
+                                EXPERTISE
+                            </Link>
+                            <Link 
+                                to='portfolio' 
+                                spy={true} 
+                                smooth={true} 
+                                offset={-100} 
+                                duration={500} 
+                                className={`desktopMenuListItem ${activeLink === 'portfolio' ? 'active' : ''}`}
+                                onClick={() => { closeMenu(); handleSetActive('portfolio'); }}
+                            >
+                                PORTFOLIO
+                            </Link>
+                            <Link 
+                                to='contact' 
+                                spy={true} 
+                                smooth={true} 
+                                offset={-200} 
+                                duration={500} 
+                                className={`desktopMenuListItem ${activeLink === 'contact' ? 'active' : ''}`}
+                                onClick={() => { closeMenu(); handleSetActive('contact'); }}
+                            >
+                                CONTACT
+                            </Link>
                         </div>
                     </ul>
                     <div className="menu-btn" onClick={toggleMenu}>
